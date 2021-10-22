1ebf63c295ef319522afaafa7e3921481c2bc9d9
TrucMy0803 <nttrucmy1998@gmail.com>
2021-10-22 23:14:56 +0700

    axios api product

---
+++ src/apis/productApi.js
@@ -0,0 +1,10 @@
+import { AxiosService } from "../services/axiosService";
+
+export const getAllProducts = () => AxiosService.get("/products");
+export const createProduct = (data) => AxiosService.post("/products", data);
+export const getProductById = (productId) => AxiosService.get(`/products}/${productId}`);
+export const updateProduct = (productId, data) => AxiosService.put(`/products/${productId}`, data);
+export const deleteProduct = (productId) => AxiosService.delete(`/products/${productId}`);
+export const getTopHighestPrice = () => AxiosService.get("/products}/topHighestPrice");
+export const getTopHighestBids = () => AxiosService.get("/products}/topHighestBids");
+export const getTopProductNearEnd = () => AxiosService.get("/products}/topProductNearEnd");
